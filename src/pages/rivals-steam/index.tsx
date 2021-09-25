8af6bb196a77fa95e2e0acfe474081c79d435436
Nikita Stroganov <findoss@yandex.ru>
2021-09-25 14:25:26 +0300

    rivals - fix ts

--- src/pages/rivals-steam/index.tsx
+++ src/pages/rivals-steam/index.tsx
@@ -1,18 +1,5 @@
-import ReactMarkdown from 'react-markdown';
-// import { useEffect, useState } from 'react';
-
 import './style.css';
 
 export const RivalsStreamPage = () => {
-  // const [text, setText] = useState('');
-
-  // useEffect(() => {
-  //   fetch(MarkdownFile)
-  //     .then(response => response.text())
-  //     .then(text => {
-  //       setText(() => text);
-  //     });
-  // }, []);
-
   return <div className="rivals-content">Stream</div>;
 };
